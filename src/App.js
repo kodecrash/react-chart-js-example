8ab876a63312288172cd9216eb293c938588946b
prathamesh karangutkar <prathameshk73@gmail.com>
2022-01-25 15:50:39 +0530

    Adding-chart-js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import PlotChart from './components/PlotChart';
 
 function App() {
+
+  const barChartConfig = {
+    type: 'bar',
+    data: {
+      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: [2478,5267,734,784,433]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+  }
+
+  const lineChartConfig = {
+    type: 'line',
+    data: {
+      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: [2478,5267,734,784,433]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+    <div className="chart-container">
+       <h3>Bar Chart</h3>
+       <PlotChart data={barChartConfig.data} type={barChartConfig.type}/>
     </div>
+
+    <div className="chart-container">
+      <h3>Line Chart</h3>
+      <PlotChart data={lineChartConfig.data} type={lineChartConfig.type}/>
+    </div>
+  </>
   );
 }
 
